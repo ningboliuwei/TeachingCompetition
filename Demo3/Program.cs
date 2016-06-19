780403a6c06ec81f597b2603f6c7bc339c2c1f97
LIU Wei <liuwei@nbu.edu.cn>
2016-06-19 22:15:04 +0800

    minor changes

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo5
+namespace Demo4
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-		}
+            Console.Write("请输入数字 A: ");
+            double numberA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("请输入数字 B: ");
+            double numberB = Convert.ToDouble(Console.ReadLine());
+
+            Operation add = new OperationAdd();
+            double result1 = add.GetResult(numberA, numberB);
+
+            Operation sub = new OperationSub();
+            double result2 = sub.GetResult(numberA, numberB);
+
+            Operation mul = new OperationMul();
+            double result3 = mul.GetResult(numberA, numberB);
+
+            Operation div = new OperationDiv();
+            double result4 = div.GetResult(numberA, numberB);
+
+            Console.WriteLine("运算结果是: " + result1);
+            Console.ReadLine();
+        }
 	}
 }
